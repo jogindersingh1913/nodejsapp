c24db7a0f85e7a322d755433b6d4919d5eab74d6
jogindersingh1913 <49472802+jogindersingh1913@users.noreply.github.com>
2020-12-22 16:25:32 +0530

    second commit

---
+++ startApp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export NODE_ENV=production
+export DB_PRD_HOST=database-1.cecrflsdzeqv.us-east-1.rds.amazonaws.com 
+export DB_PRD_USER=postgres
+export DB_PRD_PASS=PostGres1234
+export NODE_HOST=localhost
+export NODE_PORT=80
+node /myapp/index.js&
+exit 0

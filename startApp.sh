501d125032414f17fd804dc46c42d01cdc4782dd
jogindersingh1913 <49472802+jogindersingh1913@users.noreply.github.com>
2020-12-23 21:01:58 +0530

    initail 1 push

--- startApp.sh
+++ startApp.sh
@@ -4,6 +4,6 @@ export DB_PRD_HOST=database-1.cecrflsdzeqv.us-east-1.rds.amazonaws.com
 export DB_PRD_USER=postgres
 export DB_PRD_PASS=PostGres1234
 export NODE_HOST=localhost
-export NODE_PORT=8080
+export NODE_PORT=3000
 node /myapp/index.js&
 exit 0
